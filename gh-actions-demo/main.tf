c86e0e95f5cd81af6d98247879c07443be70ce98
Prem Subash <prem.subash@snowflake.com>
2022-08-31 16:26:14 -0500

    Create main.tf

---
+++ gh-actions-demo/main.tf
@@ -0,0 +1,23 @@
+#sample tf
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.35"
+    }
+  }
+}
+
+provider "snowflake" {
+  role  = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name     = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "small"
+  auto_suspend = 60
+}
