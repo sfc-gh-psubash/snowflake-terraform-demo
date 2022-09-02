1e4218058ba785673d9f8741487622e0809a45f3
Prem Subash <prem.subash@snowflake.com>
2022-09-02 10:45:06 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.43.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "psubash-build-2022"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "tf_demo_db22" {
+  name    = "TF_DEMO_DB22"
+  comment = "Database for Snowflake Terraform demo"
+}
